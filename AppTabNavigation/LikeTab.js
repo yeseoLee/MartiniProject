72f828edc525c7ea542a95f006aa2cda5fefa94a
bomin <bomin1017@naver.com>
2021-01-19 17:27:33 +0900

    책 상세정보 페이지 수정중

--- AppTabNavigation/LikeTab.js
+++ AppTabNavigation/LikeTab.js
@@ -16,7 +16,8 @@ const DATA = [
      img: require('./Like/bookImg.jpg'),
      phone: '01066285506',
      publisher: '세종출판사',
-     bookCondition: '양호'
+     bookCondition: '보밍밍밍 짱짱짱 배고프지만 먹지않겠어. 저녁은 바나나 블루베리 쉐이크. 헬스장 빨리 갔다가 집 와서 학원일 해야지 히히^_^',
+     category:'전공'
     },
     {key:  uuid(),
      title: '실전 C프로그래밍',
@@ -25,7 +26,8 @@ const DATA = [
      img: require('./Profile/images/c.png'),
      phone: '01090655436',
      publisher: '세종출판사',
-     bookCondition: '양호'
+     bookCondition: '양호',
+     category:'전공'
     },
     {key:  uuid(),
      title: '알고리즘',
@@ -34,7 +36,8 @@ const DATA = [
      img: require('./Profile/images/algo.png'),
      phone: '01046785984',
      publisher: '세종출판사',
-     bookCondition: '양호'
+     bookCondition: '양호',
+     category:'전공'
     },
     {key:  uuid(),
      title: '통계학개론',
@@ -43,7 +46,8 @@ const DATA = [
      img: require('./Profile/images/statistic.png'),
      phone: '01057499394',
      publisher: '세종출판사',
-     bookCondition: '양호'
+     bookCondition: '양호',
+     category:'전공'
     },
     {key:  uuid(),
      title: '해커스 토익 RC',
@@ -52,7 +56,8 @@ const DATA = [
      img: require('./Profile/images/toeic.png'),
      phone: '01041793445',
      publisher: '세종출판사',
-     bookCondition: '양호'
+     bookCondition: '양호',
+     category:'전공'
     },
     {key:  uuid(),
      title: '미분적분학',
@@ -61,7 +66,8 @@ const DATA = [
      img: require('./Profile/images/image2.png'),
      phone: '01066176371',
      publisher: '세종출판사',
-     bookCondition: '양호'
+     bookCondition: '양호',
+     category:'전공'
     },
 ];
 
@@ -95,7 +101,7 @@ export default class LikeTab extends React.Component{
                 <View style={styles.bodyContainer}>
                     <FlatList data = {DATA}
                          renderItem={({item}) => <FavoriteBook name={item.title} className={item.className} price={item.price} img={item.img} phone={item.phone} publisher={item.publisher}
-                         bookCondition = {item.bookCondition}/>} />
+                         bookCondition = {item.bookCondition} category={item.category}/>} />
                 </View>
             </View>
 
