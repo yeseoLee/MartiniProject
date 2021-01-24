7583d213d60a5e0322a882a53281cbe0614b9b88
gmltn9233 <63588364+gmltn9233@users.noreply.github.com>
2021-01-24 17:49:35 +0900

    Update book.js

--- AppTabNavigation/Profile/book.js
+++ AppTabNavigation/Profile/book.js
@@ -67,7 +67,7 @@ export default class BookScreen extends React.Component{
                 <View>
                 <Book visible={this.state.modalVisible} closeModal = {this.closeModal.bind(this)} bookName={this.props.name} className={this.props.className} price={this.props.price}
                     publisher={this.props.publisher} bookCondition={this.props.bookCondition} img = {this.props.img}/>
-                    <TouchableOpacity  onPress={this.openModal.bind(this)}>
+                    <TouchableOpacity  onPress={this.openModal.bind(this)} disabled={this.state.ds}>
                     <Image style={styles.image}
                     source={this.props.img}/>
                     </TouchableOpacity>
