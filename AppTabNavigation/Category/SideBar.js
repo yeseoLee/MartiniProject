3777f9cfd893c793b9cdfb117310c89a5960a82f
Sunyeup-Kim <59389380+Sunyeup-Kim@users.noreply.github.com>
2021-01-24 17:28:20 +0900

    카테고리에 따가 이미지 큼
    카테고리에 목록 위에, 데싸로고를 추가했다

---
+++ AppTabNavigation/Category/SideBar.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import SafeAreaView from 'react-native-safe-area-view';
+import { DrawerItems } from 'react-navigation-drawer';
+import {View, Image, StyleSheet, ScrollView} from 'react-native';
+
+export default class SideBar extends React.Component{
+  render() {
+    return(
+    <ScrollView>
+      <SafeAreaView
+        style={styles.container}
+        forceInset={{ top: 'always', horizontal: 'never' }}
+      > 
+        <View style = {{justifyContent:'center', alignItems:'center'}}>
+          <Image style = {styles.bookImage} source = {require('../../assets/DS_Logo.png')}/>
+        </View>
+        <DrawerItems {...this.props} />
+      </SafeAreaView>
+    </ScrollView>
+    )}
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  bookImage:{
+    width: 100,
+    height:100,
+    marginBottom:20,
+  }
+});
